5057a470da917c8fe8451246f43ed51b2c910f1d
sashapoliti <sas.politi@gmail.com>
2024-05-22 17:43:47 +0200

    add function index in home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Train;
+
 class HomeController extends Controller
 {
-    //
+    public function index() {
+        $trains = Train::all();
+        return view('home', compact('trains'));
+    }
 }
