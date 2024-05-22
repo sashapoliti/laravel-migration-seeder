241e3c4634ac1d3f2fbdd98f1eaa16612eecf36e
sashapoliti <sas.politi@gmail.com>
2024-05-22 17:50:54 +0200

    add route and print trains in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,19 @@
 
 @section('content')
 <main>
-    <h1>Home page</h1>
+    <h1>Treni</h1>
+    
+        @foreach ($trains as $train)
+        <ul class="mx-5 my-5 ">
+            <li>Compagnia: {{ $train -> company }}</li>
+            <li>Partenza: {{ $train -> departure_station }}</li>
+            <li>Arrivo: {{ $train -> arrival_station }}</li>
+            <li>Treno N: {{ $train -> train_code }}</li>
+            <li>Orario partenza: {{ $train -> departure_time }}</li>
+            <li>Orario arrivo: {{ $train -> arrival_time }}</li>
+        </ul>
+        @endforeach
+    
 </main>
 
 @endsection
