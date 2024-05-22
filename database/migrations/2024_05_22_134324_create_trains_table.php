5008215fb26ca8a622082628156dab2eebfce7ca
sashapoliti <sas.politi@gmail.com>
2024-05-22 17:34:04 +0200

    change migration create, add 2 trains in db

--- database/migrations/2024_05_22_134324_create_trains_table.php
+++ database/migrations/2024_05_22_134324_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 255);
+            $table->string('departure_station', 255);
+            $table->string('arrival_station', 255);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->integer('train_code');
+            $table->tinyInteger('carriages');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
