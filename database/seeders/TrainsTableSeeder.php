f29b90ea51ff46ab1740d1fc04c0fdf7d4e3978a
sashapoliti <sas.politi@gmail.com>
2024-05-23 16:30:53 +0200

    seeded db with a single run

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -14,6 +15,16 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $new_train = new Train();
+        $new_train->company = 'Trenitalia';
+        $new_train->departure_station = 'Milano';
+        $new_train->arrival_station = 'Roma';
+        $new_train->departure_time = '13:00:00';
+        $new_train->arrival_time = '15:00:00';
+        $new_train->train_code = 12345;
+        $new_train->carriages = 5;
+        $new_train->on_time = 1;
+        $new_train->cancelled = 0;
+        $new_train->save();
     }
 }
